bc3e02b2273f2ed870b9e7b1e4313eca6e536dd9
QwertyMD <aaryandhakal2020@gmail.com>
2025-03-23 19:30:18 +0545

    add practice questions

---
+++ src/workshop04/section01/quest01.java
@@ -0,0 +1,41 @@
+package workshop04.section01;
+
+class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class Dog extends Animal {
+    String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
+
+public class quest01 {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", 3, "Golden Retriever");
+        System.out.println("Name: " + dog.getName());
+        System.out.println("Age: " + dog.getAge());
+        System.out.println("Breed: " + dog.getBreed());
+    }
+}
---
+++ src/workshop04/section01/quest02.java
@@ -0,0 +1,37 @@
+package workshop04.section01;
+
+class Shape {
+    public double getPerimeter() {
+        return 0.0;
+    }
+
+    public double getArea() {
+        return 0.0;
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class quest02 {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Perimeter of the circle: " + String.format("%.2f", circle.getPerimeter()));
+        System.out.println("Area of the circle: " + String.format("%.2f", circle.getArea()));
+    }
+}
